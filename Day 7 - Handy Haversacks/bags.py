6760581cb02b1840965b2ae7be669abe4cc1ebb3
atropos148 <tibor.h148@gmail.com>
2021-01-06 11:23:54 +0100

    Day 7 - Basic setup for rule reading
    - suprise, it's dictionaries again

---
+++ Day 7 - Handy Haversacks/bags.py
@@ -0,0 +1,37 @@
+def main():
+	part_1_test_input = '''light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.'''
+
+	test_bag_rules: dict = {}
+
+	for rule_row in part_1_test_input.split(".\n"):
+		is_color: bool = True
+		rules_for_color: str = ""
+		for rule_part in rule_row.split(" bags contain "):
+			try:
+				if is_color:
+					rules_for_color = rule_part
+					test_bag_rules[rules_for_color] = {}
+				else:
+					for bag_rule in rule_part.split(", "):
+						cleaned_bag_rule = bag_rule.split()[:-1]
+						if cleaned_bag_rule[0] != "no":
+							bag_amount = cleaned_bag_rule[0]
+							bag_color = cleaned_bag_rule[1] + " " + cleaned_bag_rule[2]
+							test_bag_rules[rules_for_color][bag_color] = bag_amount
+				is_color = not is_color
+			except ValueError:
+				pass
+
+	print(test_bag_rules)
+
+
+if __name__ == '__main__':
+	main()
