5947f80a032dcd2c427467964bf789fbe2b0e5c6
atropos148 <tibor.h148@gmail.com>
2021-01-11 10:48:12 +0100

    Day 12 - Preparation

---
+++ Day 12 - Rain Risk/navigation.py
@@ -0,0 +1,8 @@
+def main():
+	# Solution for Day 12 of Advent od Code 2020
+	# Link: https://adventofcode.com/2020/day/12
+	print("Navigation - suggested route")
+
+	
+if __name__ == '__main__':
+	main()
