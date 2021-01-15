3902fe0444f931f3c6071b076135caa33d4a75f2
atropos148 <tibor.h148@gmail.com>
2021-01-15 14:58:22 +0100

    Add code for ticket values extraction

--- Day 16 - Ticket Translation/tickets.py
+++ Day 16 - Ticket Translation/tickets.py
@@ -11,14 +11,24 @@ nearby tickets:
 7,3,47
 40,4,50
 55,2,20
-38,6,12
-'''
+38,6,12'''
 
-    clean_input_test = raw_input_test.split("\n\n")
-    fields_raw = clean_input_test[0].split("\n")
+    clean_input_test: list = raw_input_test.split("\n\n")
+
+    # fields
+    fields_raw: list = clean_input_test[0].split("\n")
     fields_dict: dict = prepare_fields_dictionary(fields_raw)
 
+    # my ticket
+    # we can ignore this for Part 1
+    my_ticket_raw: str = clean_input_test[1]
+
+    # other tickets
+    ticket_values_list = get_values_from_tickets(clean_input_test[2])
+
     print(fields_dict, len(fields_dict))
+
+    print(ticket_values_list, len(ticket_values_list))
 
 
 def prepare_fields_dictionary(fields_raw: list) -> dict:
@@ -32,6 +42,17 @@ def prepare_fields_dictionary(fields_raw: list) -> dict:
             fields_dict[field_name].append([separated_values[0], separated_values[1]])
 
     return fields_dict
+
+
+def get_values_from_tickets(tickets_list: str):
+    other_tickets_raw: list = tickets_list.split("\n")[1:]
+    ticket_values_list: list = []
+    for ticket in other_tickets_raw:
+        current_ticket_values: list = []
+        for value in ticket.split(","):
+            current_ticket_values.append(value)
+        ticket_values_list.append(current_ticket_values)
+    return ticket_values_list
 
 
 if __name__ == '__main__':
