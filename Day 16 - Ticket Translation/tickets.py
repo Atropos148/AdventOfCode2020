2f339b0c46158b142cdccbd55f6353a6ae7fd7b8
atropos148 <tibor.h148@gmail.com>
2021-01-15 16:09:51 +0100

    Add working code for test input

--- Day 16 - Ticket Translation/tickets.py
+++ Day 16 - Ticket Translation/tickets.py
@@ -1,4 +1,6 @@
 def main():
+    # Solution for Day 16 of Advent od Code 2020
+    # Link: https://adventofcode.com/2020/day/16
     print("Hello Tickets!")
     raw_input_test = '''class: 1-3 or 5-7
 row: 6-11 or 33-44
@@ -13,22 +15,7 @@ nearby tickets:
 55,2,20
 38,6,12'''
 
-    clean_input_test: list = raw_input_test.split("\n\n")
-
-    # fields
-    fields_raw: list = clean_input_test[0].split("\n")
-    fields_dict: dict = prepare_fields_dictionary(fields_raw)
-
-    # my ticket
-    # we can ignore this for Part 1
-    my_ticket_raw: str = clean_input_test[1]
-
-    # other tickets
-    ticket_values_list = get_values_from_tickets(clean_input_test[2])
-
-    print(fields_dict, len(fields_dict))
-
-    print(ticket_values_list, len(ticket_values_list))
+    compute_error_rate(raw_input_test)
 
 
 def prepare_fields_dictionary(fields_raw: list) -> dict:
@@ -50,9 +37,56 @@ def get_values_from_tickets(tickets_list: str):
     for ticket in other_tickets_raw:
         current_ticket_values: list = []
         for value in ticket.split(","):
-            current_ticket_values.append(value)
+            current_ticket_values.append(int(value))
         ticket_values_list.append(current_ticket_values)
     return ticket_values_list
+
+
+def find_invalid_values(ticket_values_list: list, fields_dict: dict) -> list:
+    invalid_values: list = []
+    for ticket in ticket_values_list:
+        for value in ticket:
+            value = int(value)
+            value_invalid_positions: int = 0
+
+            for field in fields_dict:
+                invalid_ranges: int = 0
+                for number_range in fields_dict[field]:
+                    min: int = int(number_range[0])
+                    max: int = int(number_range[1])
+
+                    if (min <= value <= max) == False:
+                        invalid_ranges += 1
+
+                if invalid_ranges == len(fields_dict[field]):
+                    value_invalid_positions += 1
+
+            if value_invalid_positions == len(fields_dict):
+                invalid_values.append(value)
+    return invalid_values
+
+
+def compute_error_rate(raw_input: str):
+    clean_input_test: list = raw_input.split("\n\n")
+
+    # fields
+    fields_raw: list = clean_input_test[0].split("\n")
+    fields_dict: dict = prepare_fields_dictionary(fields_raw)
+
+    # my ticket
+    # we can ignore this for Part 1
+    my_ticket_raw: str = clean_input_test[1]
+
+    # other tickets
+    ticket_values_list = get_values_from_tickets(clean_input_test[2])
+
+    # invalid values count
+    # test data
+    invalid_values = find_invalid_values(ticket_values_list, fields_dict)
+    total: int = 0
+    for number in invalid_values:
+        total += number
+    print(f"Invalid values: {invalid_values}, Total: {total}")
 
 
 if __name__ == '__main__':
